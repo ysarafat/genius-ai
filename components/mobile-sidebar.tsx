b1e5c4dce24682644442e75506c114f23b28a575
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-11-29 18:16:44 +0600

    feat - dashboard layout, auth and sidebar

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,20 @@
+import Sidebar from "@/components/sidebar";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
