20960d2ad91606c361fe643e8f330e0e6234333b
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-12-08 23:43:05 +0600

    design upgrade modal

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
 import { MAX_FREE_COUNTS } from "@/constants";
+import { useProModal } from "@/hooks/use-pro-modal";
 import { Zap } from "lucide-react";
 import { useEffect, useState } from "react";
 
@@ -12,7 +13,7 @@ interface FreeCounterProps {
 }
 const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
   const [mounted, setMounted] = useState(false);
-
+  const proModal = useProModal();
   useEffect(() => {
     setMounted(true);
   }, []);
@@ -32,7 +33,13 @@ const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
               value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
             />
           </div>
-          <Button className="w-full" variant="premium">
+          <Button
+            className="w-full"
+            variant="premium"
+            onClick={() => {
+              proModal.onOpen();
+            }}
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
